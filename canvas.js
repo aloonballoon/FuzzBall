f73cd01216abfb33cd549b33caa8c738a1104b8b
Alan Uraz <alanuraz@gmail.com>
2018-07-25 17:55:02 -0400

    Bounce balls off of each other

--- canvas.js
+++ canvas.js
@@ -69,6 +69,59 @@ const getRandomColor = () => {
 //   context.stroke();
 // }
 
+const resolveCollision = (particle1, particle2) => {
+    const deltaVelocityX = particle1.velocity.x - particle2.velocity.x;
+    const deltaVelocityY = particle1.velocity.y - particle2.velocity.y;
+
+    const xDist = particle2.x - particle1.x;
+    const yDist = particle2.y - particle1.y;
+
+
+    if (deltaVelocityX * xDist + deltaVelocityY * yDist >= 0) {
+
+
+        const angle = -Math.atan2(particle2.y - particle1.y, particle2.x - particle1.x);
+
+        const m1 = particle1.mass;
+        const m2 = particle2.mass;
+
+        const u1 = rotate(particle1.velocity, angle);
+        const u2 = rotate(particle2.velocity, angle);
+
+        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
+        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+
+        const vFinal1 = rotate(v1, -angle);
+        const vFinal2 = rotate(v2, -angle);
+
+        particle1.velocity.x = vFinal1.x;
+        particle1.velocity.y = vFinal1.y;
+
+        particle2.velocity.x = vFinal2.x;
+        particle2.velocity.y = vFinal2.y;
+    }
+};
+
+const rotate = (velocity, angle) => {
+    const rotatedVelocities = {
+        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+    };
+
+    return rotatedVelocities;
+};
+
+class Line {
+  constructor(direction) {
+    this.direction = direction;
+  }
+
+  draw() {
+    this.stroke
+  }
+}
+
+
 
 
 
@@ -77,71 +130,105 @@ class Circle {
   constructor(x, y, dx, dy, radius, color) {
     this.x = x;
     this.y = y;
-    this.dx = dx;
+    this.velocity = {
+      x: dx,
+      y: dy
+    };
     this.dy = dy;
     this.radius = radius;
     this.color = color;
+    this.mass = 1;
   }
 
   draw() {
-    context.fillStyle = this.color;
-    context.fill();
+    context.strokeStyle = this.color;
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
     context.stroke();
   }
 
-  update() {
+  update(particles) {
+
+    for (let j = 0; j < particles.length; j++) {
+      if (this === particles[j]){
+       continue;
+      }
+      if (getDistance(this.x, this.y, particles[j].x, particles[j].y) - particles[j].radius * 2 < 0) {
+          resolveCollision(this, particles[j]);
+      }
+    }
+
     if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
-      this.dx = -this.dx;
+      this.velocity.x = -this.velocity.x;
     }
 
     if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
-      this.dy = -this.dy;
+      this.velocity.y = -this.velocity.y;
     }
 
-    this.x += this.dx;
-    this.y += this.dy;
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
 
     this.draw();
   }
 
-}
+ }
+
 
 let circle1;
-let circle3;
+let circle2;
+let particles;
 
 const init = () => {
-  circle1 = new Circle(300, 300, 5, 5, 100, 'black');
-  circle3 = new Circle(undefined, undefined, 0, 0, 100, 'red');
+  particles = [];
+
+  for (let i = 0; i < 20; i++) {
+    let color = getRandomColor();
+    let radius = 50;
+    let x = Math.random() * (canvas.width - radius * 2) + radius;
+    let y = Math.random() * (canvas.height - radius * 2) + radius;
+    let dx = (Math.random() - 0.5) * 10;
+    let dy = (Math.random() - 0.5) * 10;
+
+    if (i !== 0) {
+      for (let j = 0; j < particles.length; j++) {
+        if (getDistance(x, y, particles[j].x, particles[j].y) -radius * 2 < 0) {
+          x = Math.random() * (canvas.width - radius * 2) + radius;
+          y = Math.random() * (canvas.height - radius * 2) + radius;
+          j = -1;
+        }
+      }
+    }
+  particles.push(new Circle(x, y, dx, dy, radius, color));
+
+  }
 };
 
 
-let circleArr = [];
 
-// for (let i = 0; i < 40; i++) {
-//   let color = getRandomColor();
-//   let radius = 10;
-//   let x = Math.random() * (canvas.width - radius * 2) + radius;
-//   let y = Math.random() * (canvas.height - radius * 2) + radius;
-//   let dx = (Math.random() - 0.5) * 2;
-//   let dy = (Math.random() - 0.5) * 2;
-//   circleArr.push(new Circle(x, y, dx, dy, radius, color));
-// }
+
+const getDistance = (x1,y1, x2, y2) => {
+  let xDistance = x2 - x1;
+  let yDistance = y2 - y1;
+
+  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+};
 
 
 
 const animate = () => {
     requestAnimationFrame(animate);
     context.clearRect(0, 0, innerWidth, innerHeight);
-    circle1.update();
-    circle3.x = mouse.x;
-    circle3.y = mouse.y;
-    circle3.update();
 
-    // for (let i = 0; i < circleArr.length; i++) {
-    //   circleArr[i].update();
+    // if (getDistance(circle1.x, circle1.y, circle2.x, circle2.y) < circle1.radius + circle2.radius) {
+    //   circle1.color = "purple";
+    // } else if (getDistance(circle1.x, circle1.y, circle2.x, circle2.y) > circle1.radius + circle2.radius) {
+    //   circle1.color = 'yellow';
     // }
+
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].update(particles);
+    }
   };
 
   init();
