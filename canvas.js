cdd4f7001011bbd9efae4bc8c0343589921586cf
Alan Uraz <alanuraz@gmail.com>
2018-07-25 13:11:05 -0400

    Create canvas element with random balls bouncing off walls

---
+++ canvas.js
@@ -0,0 +1,115 @@
+const canvas = document.querySelector('canvas');
+
+canvas.width = 1000;
+canvas.height = 700;
+
+const context = canvas.getContext('2d');
+
+// context.fillStyle = 'rgba(0, 255, 0, 1)';
+// context.fillRect(100, 100, 100, 100);
+// context.fillStyle = 'blue';
+// context.fillRect(300, 100, 100, 100);
+// context.fillRect(300, 300, 100, 100);
+// context.fillRect(100, 300, 100, 100);
+// context.fillStyle = 'purple';
+// context.fillRect(200, 200, 100, 100);
+
+//line
+// context.beginPath();
+// context.moveTo(50, 500);
+// context.lineTo(300, 600);
+// context.lineTo(400, 700);
+// context.strokeStyle = 'pink';
+// context.stroke();
+
+//arc
+// context.strokeStyle = 'green';
+// context.beginPath();
+// context.arc(500, 500, 100, 0, Math.PI * 2, false);
+// context.stroke();
+
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+//
+// for (let i = 0; i < 10; i++) {
+//   let x = Math.random() * window.innerWidth;
+//   let y = Math.random() * window.innerHeight;
+//   context.strokeStyle = getRandomColor();
+//   context.beginPath();
+//   context.arc(x, y, 100, 0, Math.PI * 2, false);
+//   context.stroke();
+// }
+
+
+
+
+class Circle {
+
+  constructor(x, y, dx, dy, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    context.strokeStyle = this.color;
+    context.fillStyle = this.color;
+    context.fill();
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    context.stroke();
+  }
+
+  update() {
+    if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+    }
+
+    if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  }
+
+}
+
+
+
+let circleArr = [];
+
+for (let i = 0; i < 40; i++) {
+  let color = getRandomColor();
+  let radius = 10;
+  let x = Math.random() * (canvas.width - radius * 2) + radius;
+  let y = Math.random() * (canvas.height - radius * 2) + radius;
+  let dx = (Math.random() - 0.5) * 2;
+  let dy = (Math.random() - 0.5) * 2;
+  circleArr.push(new Circle(x, y, dx, dy, radius, color));
+}
+
+
+
+const animate = () => {
+    requestAnimationFrame(animate);
+    context.clearRect(0, 0, innerWidth, innerHeight);
+
+    for (let i = 0; i < circleArr.length; i++) {
+      circleArr[i].update();
+    }
+
+  };
+
+  animate();
